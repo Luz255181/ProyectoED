a488456ae48fb1f986fb418ddd0212782ba139dd
Gonzalo Perez <zalogonpe@hotmail.com>
2019-10-04 20:07:18 -0300

    Cambios en clase Paciente
    Agregue unos métodos en la clase Paciente y agregue unos comentarios.

--- src/Logica/Paciente.java
+++ src/Logica/Paciente.java
@@ -1,21 +1,42 @@
 package Logica;
-
-public class Paciente 
-{
-	protected String FN;
+/**
+ * La clase Paciente modela a un paciente hospedado en la sala de urgencias del hospital.
+ * Por cada paciente creado se lleva registro de su DNI, obra social, fecha de nacimiento y
+ * la habitación del hospital en la que se ocupa dicho paciente.
+ * @author Luz Cabral y Gonzalo Perez
+ *
+ */
+public class Paciente {
+	
+	//Atributos de instancia
+	protected int DNI;
+	protected String fechaNacimiento;
 	protected String OS;
 	protected char habitacion;
 	
-	public Paciente(String fecha, String obra, char hab)
+	public Paciente(int dni, String fecha, String obra, char hab)
 	{
-		FN = fecha;
+		DNI = dni;
+		fechaNacimiento = fecha;
 		OS = obra;
 		habitacion = hab;
 	}
 	
+	//Comandos
+	public void setOS(String obra) {
+		OS = obra;
+	}
+	public void setHabitacion(char hab) {
+		habitacion = hab;
+	}
+	
+	//Consultas
+	public int getDni() {
+		return DNI;
+	}
 	public String getFechaNacimiento()
 	{
-		return FN;
+		return fechaNacimiento;
 	}
 	
 	public String getObraSocial()
